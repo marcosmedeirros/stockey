d5652767856a212b8f63e4f58d0c94f15ff107ca
Marcos Medeiros <109443751+marcosmedeirros@users.noreply.github.com>
2024-11-06 15:49:05 -0300

    Criação do Repository para buscar todos os produtos de uma determinada categoria

--- src/main/java/br/csi/stockey/Models/ProdutoRepository.java
+++ src/main/java/br/csi/stockey/Models/ProdutoRepository.java
@@ -2,10 +2,13 @@ package br.csi.stockey.Models;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
     public Produto findProdutoByUuid(UUID uuid);
     public void deleteProdutoByUuid(UUID uuid);
+
+    List<Produto> findAllByCategoriaIdcategoria(Long idcategoria);
 }
